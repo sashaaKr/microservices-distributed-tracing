3e82020f239b278448492a24657f7298655bc593
Sasha <sasha@Sashas-MacBook-Pro-2.local>
2020-09-12 07:20:09 +0300

    add lib

--- go/bigbrother/main.go
+++ go/bigbrother/main.go
@@ -6,4 +6,45 @@ import (
   "net/http"
   "strings"
 
-  open
+  opentracing "github.com/opentracing/opentracing-go"
+  otlog "github.com/opentracing/opentracing-go/log"
+  "github.com/ssashkr/microservices-distributed-tracing/go/lib/tracing"
+  "github.com/ssashkr/microservices-distributed-tracing/go/lib/people"
+)
+
+var repo *people.Repository
+
+func main() {
+  tracer, closer := tracing.Init("bigbrother")
+  defer closer.Close()
+  opentracing.SetGlobalTracer(tracer)
+
+  repo = people.NewRepository()
+  defer repo.Close()
+
+  http.HandleFunc("/getPerson/", handleGetPerson)
+
+  log.Pring("Listening on http://localhost:8081/")
+  log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func handleGetPerson(w http.ResponseWriter, r *http.Request) {
+  spanCtx, _ := opentracing.GlobalTracer().Extract(
+    opentracing.HTTPHeaders,
+    opentracing.HTTPHeadersCarrier(r.Header),
+  )
+  span := opentracing.GlobalTracer().StartSpan(
+    "/getPerson",
+    opentracing.ChildOd(spanCtx),
+  )
+  defer span.Finish()
+
+  ctx := opentracing.ContextWithSpan(r.Context, span)
+
+  name := strings.TrimPrefix(r.URL.Path, "/getPerson/")
+  person, err := repo.GetPerson(ctx, name)
+  if err != nil {
+  }
+
+}
+
